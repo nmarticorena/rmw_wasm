cfa767638d76891b33b6283a461c04c00621a46c
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2025-03-24 09:51:20 +1000

    Try to output directly on jupyter

--- wasm_cpp/include/wasm_cpp/modes.hpp
+++ wasm_cpp/include/wasm_cpp/modes.hpp
@@ -1,5 +1,7 @@
 #include <emscripten.h>
+#include <emscripten/console.h>
 #include <string>
+#include <iostream>
 #ifndef WASM_CPP__MODES_HPP_
 #define WASM_CPP__MODES_HPP_
 
@@ -11,11 +13,17 @@ inline bool roslibjs_enable() {
     }
     std::string value(enabled);
     if (value == "1") {
+
+        emscripten_out("ROSLIBJS_ENABLE 1");
         return true;
     } else if (value == "0") {
+
+        emscripten_out("ROSLIBJS_ENABLE 0");
         return false;
     }
     else{
+        std::cout << "ROSLIBJS_ENABLE Should be 1 or 0, defaulting to off cout version";
+        emscripten_out("ROSLIBJS_ENABLE Should be 1 or 0, defaulting to off");
         EM_ASM({console.log("ROSLIBJS_ENABLE Should be 1 or 0, defaulting to off");});
         return false;
     }
